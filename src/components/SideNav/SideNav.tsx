40e4f4c6d39e20bdf84c70d7ccd0913d5b51ea94
Boris Cherny <boris@performancejs.com>
2018-05-20 12:16:34 -0700

    add github link

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { ExternalLink, Github } from 'react-feather'
 import { ROUTES } from '../../constants'
 import { withStore } from '../../services/store'
 import { Logo } from '../Logo/Logo'
@@ -14,20 +15,21 @@ export let SideNav = withStore(({store}) =>
         <li key={route}>
           <a
             className={route === store.get('route')[0] && (!store.get('route')[1] || !subroutes.length) ? '-Active' : ''}
-            onClick={() => store.set('route')([route])}
+            href={'#' + route}
           >{text}</a>
           <ul>
             {subroutes.map(([subroute, subtext]) =>
               <li key={subroute}>
                 <a
                   className={store.get('route')[1] && route === store.get('route')[0] && subroute === store.get('route')[1] ? '-Active' : ''}
-                  onClick={() => store.set('route')([route, subroute])}
+                  href={'#' + route + '/' + subroute}
                 >{subtext}</a>
               </li>
             )}
           </ul>
         </li>
       )}
+      <li><a href='https://github.com/bcherny/undux'><Github />Github<ExternalLink className='Right' /></a></li>
     </ul>
   </nav>
 )
