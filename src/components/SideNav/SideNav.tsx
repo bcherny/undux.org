a6e3d44d14548b08aeefc2348a976427d5e65e15
Boris Cherny <boris@performancejs.com>
2018-05-20 12:02:14 -0700

    visually activate active subroute in sidenav

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -13,13 +13,16 @@ export let SideNav = withStore(({store}) =>
       {ROUTES.map(([route, text, subroutes]) =>
         <li key={route}>
           <a
-            className={route === store.get('route')[0] ? '-Active' : ''}
+            className={!store.get('route')[1] && route === store.get('route')[0] ? '-Active' : ''}
             onClick={() => store.set('route')([route])}
           >{text}</a>
           <ul>
             {subroutes.map(([subroute, subtext]) =>
               <li key={subroute}>
-                <a onClick={() => store.set('route')([route, subroute])}>{subtext}</a>
+                <a
+                  className={store.get('route')[1] && route === store.get('route')[0] && subroute === store.get('route')[1] ? '-Active' : ''}
+                  onClick={() => store.set('route')([route, subroute])}
+                >{subtext}</a>
               </li>
             )}
           </ul>
