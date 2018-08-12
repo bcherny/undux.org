f3588d3df995f61097e202b5fa8039c16b693088
Boris Cherny <boris@performancejs.com>
2018-08-11 19:27:49 -0700

    improve mobile layout

--- src/components/LanguageToggler/LanguageToggler.tsx
+++ src/components/LanguageToggler/LanguageToggler.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react'
 import { Language } from '../../datatypes'
 import { withStore } from '../../services/store'
+import { invert } from '../../utils'
 import { Tabs } from '../Tabs/Tabs'
+import './LanguageToggler.css'
 
 let languages: Language[] = [
   'TypeScript',
@@ -10,12 +12,37 @@ let languages: Language[] = [
   'JavaScript (ES5)'
 ]
 
+let shortLanguages = [
+  'TS',
+  'Flow',
+  'ES6',
+  'ES5'
+]
+
+let languageToShortLanguage: Record<Language, string> = {
+  TypeScript: 'TS',
+  Flow: 'Flow',
+  'JavaScript (ES6)': 'ES6',
+  'JavaScript (ES5)': 'ES5'
+}
+
+let shortLanguageToLanguage = invert(languageToShortLanguage)
+
 export let LanguageToggler = withStore(({ store }) =>
   <div className='LanguageToggler'>
-    <Tabs
-      activeItem={store.get('language')}
-      items={languages}
-      onClick={store.set('language')}
-    />
+    <div className='HideOnMobile'>
+      <Tabs
+        activeItem={store.get('language')}
+        items={languages}
+        onClick={store.set('language')}
+      />
+    </div>
+    <div className='ShowOnMobile'>
+      <Tabs
+        activeItem={languageToShortLanguage[store.get('language')]}
+        items={shortLanguages}
+        onClick={_ => store.set('language')(shortLanguageToLanguage[_])}
+      />
+    </div>
   </div>
 )
---
+++ src/utils.ts
@@ -0,0 +1,8 @@
+export function invert<
+  A extends string | number | symbol,
+  B extends string | number | symbol
+>(r: Record<A, B>): Record<B, A> {
+  let o: Record<B, A> = {} as any
+  Object.keys(r).forEach(k => (o as any)[r[k]] = k)
+  return o
+}
